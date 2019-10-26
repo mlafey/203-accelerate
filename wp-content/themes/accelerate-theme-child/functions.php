47dcc5ab112a24596e4d1f217e27146021a59e88
Mariah <mariah.lafey@gmail.com>
2019-10-25 19:39:27 -0600

    updating

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -13,6 +13,25 @@
 // Enqueue scripts and styles
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+  wp_enqueue_style('accelerate-child-font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array('child-style'));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+
+// Add Custom Post - Case Studies
+function create_custom_post_types() {
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+				'singular_name' => __( 'Case Study' )   
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies'),       
+        )
+    );   
+}
+add_action ( 'init', 'create_custom_post_types' );
+
--- wp-content/themes/accelerate-theme/page.php
+++ wp-content/themes/accelerate-theme/page.php
@@ -14,7 +14,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
+	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
