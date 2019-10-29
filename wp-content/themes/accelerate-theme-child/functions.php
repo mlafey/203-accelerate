c32b0fc33c906ebd321e4c1546184f241e3996bc
Mariah <mariah.lafey@gmail.com>
2019-10-29 14:04:53 -0600

    ignoring uploads content

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -35,3 +35,11 @@ function create_custom_post_types() {
 }
 add_action ( 'init', 'create_custom_post_types' );
 
+add_filter(
+    'body_class','accelerate_child_body_classes' );
+function accelerate_child_body_classes ($classes ) {
+    if (is_page('contact') ) {
+        $classes[] = 'contact';
+        }
+    return $classes;
+}
